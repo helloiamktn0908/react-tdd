e6262fe7b86f1683b70fc9e333446aa8dedc2140
tk1351 <takakikitsui0111@gmail.com>
2022-01-16 10:54:03 +0900

    feat: RTLの動作確認

---
+++ app/src/utils/Rtl.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Rtl() {
+    const [value, setValue] = useState<string>('')
+    const [disabled, setDisabled] = useState<boolean>(true)
+
+    const onChange = (event: { target: HTMLInputElement }) => {
+        setValue(event.target.value)
+        setDisabled(false)
+    }
+    return (
+        <>
+            <input type='text' value={value} aria-label='test-input' onChange={onChange} />
+            <button type='button' aria-label='test-button' disabled={disabled}>Submit</button>
+        </>
+    )
+}
+
+export default  Rtl
